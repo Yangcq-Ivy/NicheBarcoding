222e34f877559e6ca9092f1a5c53a84b1385322a
Yangcq-Ivy <69965806+Yangcq-Ivy@users.noreply.github.com>
2024-08-08 19:46:57 +0800

    Delete tests directory

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(NicheBarcoding)
-
-test_check("NicheBarcoding")
--- tests/testthat/test-monophyly.prop.R
+++
@@ -1,9 +0,0 @@
-# Test Start ###
-test_that("class testing on output",{
-  library(ape)
-  tree<-ape::rtree(20)
-  tree$tip.label<-sample(tree$tip.label[1:10],size=20,replace = TRUE)
-  sppVector<-tree$tip.label
-
-  expect_equal(length(monophyly.prop(tree,sppVector,singletonsMono=F)),4)
-})
